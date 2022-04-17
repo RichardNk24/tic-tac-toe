5d30fb98eaacdef5dbc8ace72edbfeef6332f0a5
Richard Nkulu <64165035+RichardNk24@users.noreply.github.com>
2022-04-17 20:13:31 +0200

    Update game.py

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ from player import HumanPlayer, RandomComputerPlayer
 
 class TicTacToe:
     def __init__(self):
-        self.board = [" " for _ in                                                                                                        range(9)]
+        self.board = [" " for _ in                                                                                                       range(9)]
         self.current_winner = None
         
     def print_board(self):
