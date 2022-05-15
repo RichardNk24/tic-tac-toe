c850325e3fa622b4ac42fe7c9daa21c0a50f738f
Richard Nkulu <64165035+RichardNk24@users.noreply.github.com>
2022-05-15 10:49:17 +0200

    Update player.py

--- player.py
+++ player.py
@@ -108,3 +108,9 @@ class HumanPlayer(Player):
     ValueError: invalid literal for int() with base 10: '2+3'
     >>> eval('2+3')
     5
+    
+    >>> x = 12.3456789
+    >>> print('The value of x is %3.2f' %x)
+    The value of x is 12.35
+    >>> print('The value of x is %3.4f' %x)
+    The value of x is 12.3457
