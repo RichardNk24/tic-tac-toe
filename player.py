bb072240b4435e2efe20e5c1b53a587d06a27306
Richard Nkulu <64165035+RichardNk24@users.noreply.github.com>
2022-05-15 10:59:31 +0200

    Update player.py

--- player.py
+++ player.py
@@ -114,3 +114,24 @@ class HumanPlayer(Player):
     The value of x is 12.35
     >>> print('The value of x is %3.4f' %x)
     The value of x is 12.3457
+    
+    x = 15
+    y = 4
+
+    # Output: x + y = 19
+    print('x + y =',x+y)
+
+    # Output: x - y = 11
+    print('x - y =',x-y)
+
+    # Output: x * y = 60
+    print('x * y =',x*y)
+
+    # Output: x / y = 3.75
+    print('x / y =',x/y)
+
+    # Output: x // y = 3
+    print('x // y =',x//y)
+
+    # Output: x ** y = 50625
+    print('x ** y =',x**y)
